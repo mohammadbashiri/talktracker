fa15b0195d2b9fc398d0ecf1f70b70e6a9f500f5
Aleksandra Teska <aleksandra.teska@gmail.com>
2018-10-13 19:05:43 +0200

    basic add Member gui edited

--- examples/flexx_gui.py
+++ examples/flexx_gui.py
@@ -3,37 +3,30 @@ import talktracker as tt
 
 class Example(flx.PyComponent):
 
-    def setup_member(self, msg):
-        print(msg)
-        mem = tt.Member(msg)
-
-
     def init(self):
         super().init()
         with flx.VBox():
             with flx.HBox():
-                self.sessionname = flx.LineEdit(placeholder_text='First name')
+                self.member_name = flx.LineEdit(placeholder_text='Member name')
+
             with flx.HBox():
-                self.but = flx.Button(text='Reset')
-                self.butadd = flx.Button(text='Add')
-                self.label = flx.Label(flex=1)
+                self.but_create = flx.Button(text='Create')
+                self.label_member = flx.Label(flex=1)
 
 
-    @flx.reaction('sessionname.text')
-    def greet(self, *events):
-        self.label.set_text(self.sessionname.text)
+    # @flx.reaction('member_name.text')
+    # def greet(self, *events):
+    #     self.label_member.set_text(self.member_name.text)
 
-    @flx.reaction('but.pointer_click')
-    def reset(self, *events):
-        self.label.set_text('')
+    @flx.reaction('but_create.pointer_click')
+    def create_member(self, *events):
+        mem = tt.Member(self.member_name.text)
+        self.label_member.set_text('Member ' + self.member_name.text + ' created')
 
-    @flx.reaction('butadd.pointer_click')
-    def reset(self, *events):
-        self.setup_session(self.sessionname.text)
 
 
 if __name__ == '__main__':
     app = flx.App(Example)
-    app.launch('app')  # to run as a desktop app
-    # app.launch('browser')  # to open in the browser
+    app.launch('app')
+    # app.launch('browser')
 flx.run()
